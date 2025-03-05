8b79f7a7a1edf4a211a68e8d9fcc91fa8991b3df
TummojuAkhila <akhilatummoju2004@gmail.com>
2025-03-05 09:39:55 +0530

    adding all files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/ban.png" class="img-fluid w-100 pt-1" alt="..."/>
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/pasta.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Pasta</h5>
+        <p class="card-text">fill a large pot with water, bring it to a boil, add salt, then add the pasta and cook until "al dente" (firm to the bite) according to package instructions, finally drain the pasta and serve with your desired sauce; remember to reserve some pasta water to add to your sauce for extra creaminess. </p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/pulao.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">veg pulao</h5>
+        <p class="card-text">Heat oil in a pan, add cumin seeds, bay leaf, and cinnamon stick.Add sliced onions and sauté until golden brown.Add chopped tomatoes and cook until soft.Mix in turmeric powder, salt, garam masala, and chopped vegetables.Add washed rice and sauté for a minute.Pour in 2 cups of water, cover, and cook on low heat for 15 minutes.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/paneerbuttermasala.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Paneer Butter Masala</h5>
+        <p class="card-text">Heat butter in a pan and sauté chopped onions until golden.Add ginger-garlic paste and cook for a minute.Pour in tomato puree and cook until the oil separates.Add chili powder, turmeric, garam masala, and salt.Add paneer cubes and mix well.Pour in fresh cream, stir, and cook for 2 minutes.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="chocolatemugcake.png" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">chocolatemugcake</h5>
+        <p class="card-text">Mix all dry ingredients in a mug.Add milk, oil, and vanilla extract; mix well.Microwave for 1-2 minutes until the cake rises.Let it cool slightly and enjoy!
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
